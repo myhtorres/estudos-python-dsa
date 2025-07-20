76928eb5a1f89b11cfdd9f60b7a87017d9c2ed45
Myrelle Torres <108523959+myhtorres@users.noreply.github.com>
2025-07-20 11:59:36 -0300

    Create CAP04-NUMEROS.py

---
+++ CAP04-NUMEROS.py
@@ -0,0 +1,108 @@
+Operações Aritméticas
+
+# Soma
+4 + 4
+8
+
+# Subtração
+4 - 3
+1
+
+# Multiplicação
+3 * 3
+9
+
+# Divisão
+3 / 2
+1.5
+
+# Potência
+4 ** 2  
+16
+
+# Módulo [é o resto da divisão, é o que sobra]
+10 % 3  
+1
+Função Type - p/ quando quiser saber o tipo de um dado, objeto, estrutura
+
+type(5)
+int
+
+type(5.0)
+float
+
+a = 'Estou estudando na Data Science Academy'
+type(a)
+str
+Operações com Números Float
+
+3.1 + 6.4
+9.5
+
+4 + 4.0
+8.0
+
+4 + 4
+8
+
+# Resultado é um número float
+4 / 2
+2.0
+
+# Resultado é um número inteiro
+4 // 2
+2
+
+4 / 3.0
+1.3333333333333333
+
+4 // 3.0
+1.0
+Conversão
+
+float(9)
+9.0
+
+int(6.0)
+6
+
+int(6.5)
+6
+
+int(6.6)
+6
+Hexadecimal e Binário
+
+hex(394)
+'0x18a'
+
+hex(217)
+'0xd9'
+
+bin(286)
+'0b100011110'
+
+bin(390)
+'0b110000110'
+Funções abs, round e pow
+
+# Retorna o valor absoluto
+abs(-8)
+8
+
+# Retorna o valor absoluto
+abs(8)
+8
+
+# Retorna o valor com duas casas decimais
+round(3.14151922, 2)
+3.14
+
+# Potência
+pow(4,2)
+16
+
+# Potência
+pow(5,3)
+125
+Fim
